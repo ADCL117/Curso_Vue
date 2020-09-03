1eebb63327475caaaf9f346b1cfbf577dc648b2f
ADCL117 <adcl_halo@hotmail.com>
2020-09-02 21:48:42 -0500

    Se agregan los archivos de la práctica del video número 21

--- 15hello-worldui/src/store/index.js
+++ 15hello-worldui/src/store/index.js
@@ -2,8 +2,21 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    frutas: [
+      {nombre: 'Manzana', cantidad: 0},
+      {nombre: 'Pera',    cantidad: 0},
+      {nombre: 'Platano', cantidad: 0}
+    ]
   },
   mutations: {
+    aumentar(state, index){
+      state.frutas[index].cantidad++
+    },
+    reiniciar(state, index){
+      state.frutas.forEach(fruta => {
+        fruta.cantidad=0
+      })
+    }
   },
   actions: {
   },
