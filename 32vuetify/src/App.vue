b6075905d11cc24737ef4904a64a0081c0a39bb1
ADCL117 <adcl_halo@hotmail.com>
2020-09-10 19:56:23 -0500

    Se agregan los archivos de la práctica del video número 33

--- 32vuetify/src/App.vue
+++ 32vuetify/src/App.vue
@@ -38,7 +38,8 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <router-view>
+      </router-view>
     </v-main>
   </v-app>
 </template>
@@ -48,10 +49,6 @@ import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
 
   data: () => ({
     //
---
+++ 32vuetify/src/views/Grid.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12" sm="8">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam adipisci, dolorem voluptatibus a perferendis
+                officiis voluptatem, quisquam omnis dolores expedita soluta similique velit quas ratione fugit? Rerum possimus
+                ut natus!</p>
+            </v-col>
+
+            <v-col cols="12" sm="4">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam adipisci, dolorem voluptatibus a perferendis
+                officiis voluptatem, quisquam omnis dolores expedita soluta similique velit quas ratione fugit? Rerum possimus
+                ut natus!</p>
+            </v-col>
+
+      
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
