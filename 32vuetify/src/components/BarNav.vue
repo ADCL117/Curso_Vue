7b6e690d7b5fe19fa26b9914863faac3fdb1c302
ADCL117 <adcl_halo@hotmail.com>
2020-09-13 21:18:27 -0500

    Se agregan los archivos de la práctica del video número 36

---
+++ 32vuetify/src/components/BarNav.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container>
+
+            <v-app-bar app class="warning">
+
+                <v-app-bar-nav-icon @click="dibujar = !dibujar"></v-app-bar-nav-icon>
+                <v-toolbar-title>Vuetify</v-toolbar-title>
+                <v-spacer></v-spacer>
+
+                <v-btn class="success">ingreso</v-btn>
+                <v-btn class="error">salir</v-btn>
+            </v-app-bar>
+
+            <v-navigation-drawer app v-model="dibujar" temporary dark>
+                <v-layout mt-4 column align-center>
+                    <v-flex>
+                        <v-avatar>
+                            <img src="https://randomuser.me/api/portraits/men/85.jpg" alt="">
+                        </v-avatar>
+                    </v-flex>
+
+                    <v-flex>
+                       <p class="white--text mt-3 headline">Juanito Pérez</p> 
+                    </v-flex>
+                </v-layout> 
+            </v-navigation-drawer>
+
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'BarNav',
+    data(){
+        return {
+            dibujar: true
+        };
+    }
+};
+</script>
