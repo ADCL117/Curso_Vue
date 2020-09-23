5ff32bf9c40e8052e951100996e39e409c2fd373
ADCL117 <adcl_halo@hotmail.com>
2020-09-22 21:42:21 -0500

    Se agregan los archivos de la práctica del video número 38

--- 33cardmaterial/src/views/Home.vue
+++ 33cardmaterial/src/views/Home.vue
@@ -14,7 +14,7 @@
             perspiciatis?</p>
           </v-card-text>
           <v-card-actions>
-            <v-btn color="info" text>Acción</v-btn>
+            <v-btn color="info" text @click="barra = true">Acción</v-btn>
           </v-card-actions>
 
         </v-card>
@@ -41,7 +41,7 @@
             perspiciatis?</p>
           </v-card-text>
           <v-card-actions>
-            <v-btn color="info" text>Acción</v-btn>
+            <v-btn color="info" text @click="dialog = true">Acción</v-btn>
           </v-card-actions>
 
         </v-card>
@@ -50,13 +50,44 @@
 
     </v-layout>
 
+    <!-- Dialog -->
+      <v-dialog v-model="dialog" width="500" persistent>
+        <v-card>
+          <v-card-title> Este es el título </v-card-title>
+          <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, temporibus esse? Et, tempore nam quam
+          fugit consectetur eaque! In quas alias vero adipisci. Eius possimus blanditiis odit. Natus,
+          cumque quia?</v-card-text>
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-btn @click="dialog = false">Cerrar</v-btn>
+          </v-card-actions>
+        </v-card>
+
+      </v-dialog>
+
+    <!-- Fin Dialaog -->
+
+    <v-dialog v-model="barra" width="500">
+      <v-card color="primary" dark>
+        <div class="text-center"> Por favor espere </div>
+        <v-progress-linear indeterminate color="white" class="mb-0">
+        </v-progress-linear >
+      </v-card>
+    </v-dialog>
+
   </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-
+  data(){
+    return {
+      dialog: false,
+      barra: false
+    }
+  }
 
 }
 </script>
